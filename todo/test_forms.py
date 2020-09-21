11aa5e92f2b4aa438ea20030da4f6d2e3bf0497b
Matt Boland <mattjamesboland@gmail.com>
2020-09-21 20:31:09 +0000

    Found other test error

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -10,7 +10,7 @@ class TestItemForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('name', form.errors.keys())
         self.assertEqual(form.errors['name'][0],
-                         'This test field is required.')
+                         'This field is required.')
 
     def test_done_field_is_not_required(self):
         form = ItemForm({'name': 'Test Todo Item'})
